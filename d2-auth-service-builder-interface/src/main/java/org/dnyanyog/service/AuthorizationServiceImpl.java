84e19596ad53b93a912ca14f742da3af33fa5c9e
govindarevanwar <govindarevanwar@gmail.com>
2024-02-19 10:26:38 +0530

    Fet: Day Assignment
    - Implement Interface segregation Principle
    - constructor overloading
    - Create a Interface for UserManagement Service

---
+++ d2-auth-service-builder-interface/src/main/java/org/dnyanyog/service/AuthorizationServiceImpl.java
@@ -0,0 +1,22 @@
+package org.dnyanyog.service;
+
+import java.util.List;
+
+import org.dnyanyog.dto.AuthorizationRequest;
+import org.dnyanyog.dto.AuthorizationResponse;
+
+public class AuthorizationServiceImpl implements AuthorizationService{
+
+	@Override
+	public AuthorizationResponse authorize(AuthorizationRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AuthorizationResponse> getAllAuthorization() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
