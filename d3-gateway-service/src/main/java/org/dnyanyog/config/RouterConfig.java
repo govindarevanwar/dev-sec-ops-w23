02d2de4f0295055b7f920c3be59381bc7bebe81f
govindarevanwar <govindarevanwar@gmail.com>
2024-02-26 23:31:07 +0530

    Fet: D4 assignment to create Gateway Service
    - Create a gateway service for user and customer management

---
+++ d3-gateway-service/src/main/java/org/dnyanyog/config/RouterConfig.java
@@ -0,0 +1,23 @@
+package org.dnyanyog.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+	
+	@Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth_service_public_route", r -> r.path("/api/v1/public/auth/**")
+                        .uri("http://localhost:8082"))
+                .route("auth_service_route", r -> r.path("/api/v1/auth/**")
+                        .uri("http://localhost:8082"))
+                .route("customer_service_route", r -> r.path("/api/v1/customer/**")
+                        .uri("http://localhost:8083"))
+                .build();
+    }
+
+}
