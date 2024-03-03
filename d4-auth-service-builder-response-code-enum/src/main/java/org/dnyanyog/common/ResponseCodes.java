968f85c51636168506818c71492981053ea47189
govindarevanwar <govindarevanwar@gmail.com>
2024-03-03 19:44:29 +0530

    Feat: Assignment D4 add enumerated resp code and message
    - Add User response code and message enumeration
    - Add Customer response code and message enumeration

---
+++ d4-auth-service-builder-response-code-enum/src/main/java/org/dnyanyog/common/ResponseCodes.java
@@ -0,0 +1,18 @@
+package org.dnyanyog.common;
+
+public class ResponseCodes {
+	
+	public static final String ADD_CUSTOMER_SUCCESS_CODE = "Success";
+	public static final String ADD_CUSTOMER_SUCCESS_MESSAGE = "Customer added successfully";
+	public static final String ADD_CUSTOMER_DUPLICATE_EMAIL_CODE = "Fail";
+	public static final String ADD_CUSTOMER_DUPLICATE_EMAIL_MESSAGE = "Customer email already exists";
+	public static final String ADD_CUSTOMER_NOT_FOUND_CODE = "Fail";
+	public static final String ADD_CUSTOMER_NOT_FOUND_MESSAGE = "Customer not found";
+	public static final String ADD_CUSTOMER_ERROR_CODE = "Fail";
+	public static final String ADD_CUSTOMER_ERROR_MESSAGE = "Failed to add customer";
+	
+	public static final String ADD_USER_SUCCESS_CODE = "Success";
+	public static final String ADD_USER_SUCCESS_MESSAGE = "User added successfully";
+	public static final String ADD_USER_ERROR_CODE = "Fail";
+	public static final String ADD_USER_ERROR_MESSAGE = "Failed to add user";
+}
