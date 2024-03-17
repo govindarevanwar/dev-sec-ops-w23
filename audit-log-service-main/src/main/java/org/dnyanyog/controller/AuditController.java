5d290bdd82658f47589ae53cad6f67c551adaaf1
govindarevanwar <govindarevanwar@gmail.com>
2024-03-17 12:00:20 +0530

    fix: Add pre-commit hooks - To format code using google formatter - To check commit messae format

---
+++ audit-log-service-main/src/main/java/org/dnyanyog/controller/AuditController.java
@@ -0,0 +1,37 @@
+package org.dnyanyog.controller;
+
+import java.util.List;
+import org.dnyanyog.dto.AuditLog;
+import org.dnyanyog.entity.AuditLogEntity;
+import org.dnyanyog.service.AuditLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuditController {
+
+  @Autowired private AuditLogService auditLogService;
+
+  @PostMapping(path = "/api/v1/audit/log")
+  public ResponseEntity<AuditLogEntity> createAuditLog(@RequestBody AuditLog auditLogRequest) {
+
+    return auditLogService.saveAuditLog(auditLogRequest);
+  }
+
+  //	    @GetMapping(path="/api/v1/audit")
+  //	    public ResponseEntity<List<AuditLog>> getAllAuditLogs() {
+  //	        return auditLogService.getAllAuditLogs();
+  //	    }
+
+  @GetMapping(path = "/api/v1/audit")
+  public ResponseEntity<List<AuditLog>> findByRequestId(
+      @RequestParam(value = "requestId") String requestId) {
+
+    return auditLogService.findByRequestId(requestId);
+  }
+}
